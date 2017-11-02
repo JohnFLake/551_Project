bc66b6d1312d1fb7699252358e912d44a2cfd23d
Ryan Hub <ryanhub@vagelos-ve502-0790.apn.wlan.upenn.edu>
2017-11-02 13:32:58 -0400

    basics for ATM client

---
+++ src/atm_client/Main.java
@@ -0,0 +1,94 @@
+package atm_client;
+
+//Package for parsing command line arguments
+import org.apache.commons.cli.*; 
+
+public class Main {
+	
+	
+	/**
+	 * Simple helper method to check if a given string is a valid port number
+	 * @param str The port String we are checking
+	 * @return Whether this is a valid port number
+	 */
+	public static boolean isValidPortNumber(String str){
+		if(!str.matches("(0|[1-9][0-9]*)")) {
+			return false; 
+		}
+		try {
+			int num = Integer.parseInt(str); 
+			if(num > 65535 || num < 1024) {
+				return false; 
+			}
+		}catch(Exception e) {
+			return false; 
+		}
+		return true; 
+	}
+
+	
+	public static void main(String[] args) {
+		
+		//Add options: 
+		Options options = new Options(); 
+		options.addOption("a", true, "The customer's account name"); 
+		options.addOption("s", true, "The authorization file"); 
+		options.addOption("i", true, "The IP address for this server to run on"); 
+		options.addOption("p", true, "The port for this server to run on"); 
+		options.addOption("c", true, "The customer's ATM card file"); 
+		CommandLineParser parser = new DefaultParser(); 
+		
+		int port = 3000; 
+		String IP = "127.0.0.1";
+		String authFile = "bank.auth"; 
+		String accountName;
+		String cardFile;
+		
+		try {
+			CommandLine cmd = parser.parse(options, args);
+			
+			// Check for account name
+			if(cmd.hasOption("a")) {
+				accountName = cmd.getOptionValue("a");
+				cardFile = accountName + ".card";
+			}
+			
+			else {
+				System.out.println("Need an account name entered");
+			}
+			
+			//Check for port option
+			if(cmd.hasOption("p")) {
+				String portString = cmd.getOptionValue("p"); 
+				if(isValidPortNumber(portString)) {
+					port = Integer.parseInt(portString); 
+				}else {
+					System.out.println("Invalid port number."); 
+				}
+			}
+			
+			//Check for auth file
+			//TODO: Add check for valid auth file. 
+			if(cmd.hasOption("s")) {
+				authFile = cmd.getOptionValue("s"); 
+			} 
+			
+			// Check for IP address
+			if(cmd.hasOption("p")) {
+				IP = cmd.getOptionValue("p"); 
+			} 
+			
+			// Check for Card File
+			if(cmd.hasOption("c")) {
+				cardFile = cmd.getOptionValue("c") + ".card"; 
+			} 
+			
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace(); 
+			System.out.println("wrong args fam.");
+		}
+	}
+
+}
