9784453a67471a085966906eae932ccedc834599
jlautman <jlautman@seas.upenn.edu>
2017-11-04 20:49:37 -0400

    Finish maven project structure setup

---
+++ Atm/src/main/java/edu/upenn/cis551/bank/Atm.java
@@ -0,0 +1,7 @@
+package edu.upenn.cis551.bank;
+
+public class Atm {
+  public static void main(String[] args) {
+    System.out.println("Hello world from the Atm");
+  }
+}
---
+++ Bank/src/main/java/edu/upenn/cis551/bank/Bank.java
@@ -0,0 +1,9 @@
+package edu.upenn.cis551.bank;
+
+public class Bank {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+  }
+
+}
