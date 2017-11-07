cf1c94ce0c21f922c304a44cf815664b64a532cf
ryanhub <ryanhub@10.0.0.134>
2017-11-06 20:58:48 -0500

    Basic skeleton for ATM the different options

--- Atm/src/main/java/edu/upenn/cis551/pncbank/Atm.java
+++ Atm/src/main/java/edu/upenn/cis551/pncbank/Atm.java
@@ -1,7 +1,42 @@
 package edu.upenn.cis551.pncbank;
 
+import org.apache.commons.cli.*; 
+
 public class Atm {
-  public static void main(String[] args) {
-    System.out.println("Hello world from the Atm");
-  }
-}
+
+			private String accountName; 
+			private String authFile;
+			private String IP;
+			private String cardFile;
+			private int port;
+			
+			public Atm(String s, String i, int p, String c, String a) {
+				this.port = p; 
+				this.authFile = s; 
+				this.accountName = a;
+				this.cardFile = c;
+				this.IP = i;
+			}
+			
+			public void CheckBalance()
+			{
+				// go into the file, check the account name and print the money they have
+			}
+			
+			public void NewAccount(int balance)
+			{
+				// add to the file the account name and intitial balance
+			}
+			
+			public void Deposit(int deposit)
+			{
+				// go to the file check the name and add money
+			}
+			
+			public void Withdrawl(int withdrawl)
+			{
+				// go to the file check the name and subtract money
+			}
+		}	
+  
+
