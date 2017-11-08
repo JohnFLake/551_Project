48077a18a0458b2f9fbe33cc6fdd8673cd6adbc9
jlautman <12954173+jlautman@users.noreply.github.com>
2017-11-08 17:06:21 -0500

    Commit empty atmservice that the bank will use

---
+++ Bank/src/main/java/edu/upenn/cis551/pncbank/bank/atmservice/AtmService.java
@@ -0,0 +1,12 @@
+package edu.upenn.cis551.pncbank.bank.atmservice;
+
+/**
+ * Accepts requests from an ATM, verifies that they are from an ATM, and then passes along any
+ * transaction requests.
+ * 
+ * @author jlautman
+ *
+ */
+public class AtmService {
+  // TODO
+}
