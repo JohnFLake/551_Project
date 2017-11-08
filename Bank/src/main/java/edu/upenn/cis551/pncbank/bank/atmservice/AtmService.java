34fb45270f0d6f7e792d287b2a288bd5740ff7f9
jlautman <12954173+jlautman@users.noreply.github.com>
2017-11-08 18:09:57 -0500

    Make sure Jackson is appropriately included and start on atm service

--- Bank/src/main/java/edu/upenn/cis551/pncbank/bank/atmservice/AtmService.java
+++ Bank/src/main/java/edu/upenn/cis551/pncbank/bank/atmservice/AtmService.java
@@ -1,5 +1,13 @@
 package edu.upenn.cis551.pncbank.bank.atmservice;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.upenn.cis551.pncbank.transaction.AbstractTransaction;
+
 /**
  * Accepts requests from an ATM, verifies that they are from an ATM, and then passes along any
  * transaction requests.
@@ -8,5 +16,37 @@ package edu.upenn.cis551.pncbank.bank.atmservice;
  *
  */
 public class AtmService {
-  // TODO
+  private ServerSocket socketBinding;
+  private ObjectMapper mapper;
+
+  public AtmService(int port) throws IOException {
+    socketBinding = new ServerSocket(port);
+    mapper = new ObjectMapper();
+  }
+
+  /**
+   * Handles the next request from the port.
+   * 
+   * @return
+   * @throws IOException
+   */
+  public void handleNextRequest() throws IOException {
+    Socket s = socketBinding.accept();
+    InputStream is = s.getInputStream();
+    // TODO Decryption of the data from the atm goes here
+    try {
+      AbstractTransaction at = mapper.readValue(is, AbstractTransaction.class);
+    } catch (IOException e) {
+      // TODO
+    }
+    // TODO Do stuff with the account store here
+    
+    String response = "{}";
+    // TODO actually encrypt
+    byte[] encryptedResponse = response.getBytes();
+    OutputStream os = s.getOutputStream();
+    os.write(encryptedResponse);
+    os.flush();
+    s.close();
+  }
 }
--- Commons/src/main/java/edu/upenn/cis551/pncbank/transaction/AbstractTransaction.java
+++ Commons/src/main/java/edu/upenn/cis551/pncbank/transaction/AbstractTransaction.java
@@ -1,5 +1,12 @@
 package edu.upenn.cis551.pncbank.transaction;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+
+@JsonSubTypes({//
+    @JsonSubTypes.Type(value = CreateAccountPOJO.class, name = "CreateAccount"),
+    @JsonSubTypes.Type(value = WithdrawPOJO.class, name = "Withdraw"),
+    @JsonSubTypes.Type(value = DepositPOJO.class, name = "Deposit")//
+})
 public class AbstractTransaction {
   final long sequenceNumber;
   final String accountName;
