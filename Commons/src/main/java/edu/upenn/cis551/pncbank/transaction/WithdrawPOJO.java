19df523bd84f87f1a0c50dfc3d36f9a0a4948430
jlautman <12954173+jlautman@users.noreply.github.com>
2017-11-08 16:42:39 -0500

    Create pojos for the three types of transaction requests from the atm to the bank

--- Commons/src/main/java/edu/upenn/cis551/pncbank/transaction/WithdrawPOJO.java
+++ Commons/src/main/java/edu/upenn/cis551/pncbank/transaction/WithdrawPOJO.java
@@ -1,7 +1,7 @@
 package edu.upenn.cis551.pncbank.transaction;
 
 public class WithdrawPOJO extends AbstractTransaction {
-  
+
   final long withdraw;
 
   public WithdrawPOJO(String accountName, long amount, long sequenceNumber) {
