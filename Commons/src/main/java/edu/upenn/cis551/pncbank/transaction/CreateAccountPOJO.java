ee3308092ae31d8251f1c1c3f36a66a9eaa446a6
jlautman <12954173+jlautman@users.noreply.github.com>
2017-11-08 16:58:38 -0500

    Encapsulate common information among transaction types

--- Commons/src/main/java/edu/upenn/cis551/pncbank/transaction/CreateAccountPOJO.java
+++ Commons/src/main/java/edu/upenn/cis551/pncbank/transaction/CreateAccountPOJO.java
@@ -1,24 +1,11 @@
 package edu.upenn.cis551.pncbank.transaction;
 
-<<<<<<< HEAD
 public class CreateAccountPOJO extends AbstractTransaction {
   final long balance;
 
   public CreateAccountPOJO(String accountName, long balance, long sequenceNumber) {
     super(accountName, sequenceNumber);
-=======
-public class CreateAccountPOJO {
-  final String accountName;
-  final long balance;
-
-  public CreateAccountPOJO(String accountName, long balance) {
-    this.accountName = accountName;
->>>>>>> Include the inital balance in the CreateAccountPOJO
     this.balance = balance;
-  }
-
-  public long getBalance() {
-    return this.balance;
   }
 
   public long getBalance() {
