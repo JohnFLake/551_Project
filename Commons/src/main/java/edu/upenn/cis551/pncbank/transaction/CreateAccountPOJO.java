ca65302be83148ec99640950fdb41498f506368b
jlautman <12954173+jlautman@users.noreply.github.com>
2017-11-08 16:52:01 -0500

    Include the inital balance in the CreateAccountPOJO

--- Commons/src/main/java/edu/upenn/cis551/pncbank/transaction/CreateAccountPOJO.java
+++ Commons/src/main/java/edu/upenn/cis551/pncbank/transaction/CreateAccountPOJO.java
@@ -2,13 +2,18 @@ package edu.upenn.cis551.pncbank.transaction;
 
 public class CreateAccountPOJO {
   final String accountName;
+  final long balance;
 
-
-  public CreateAccountPOJO(String accountName) {
+  public CreateAccountPOJO(String accountName, long balance) {
     this.accountName = accountName;
+    this.balance = balance;
   }
 
   public String getAccountName() {
     return this.accountName;
+  }
+
+  public long getBalance() {
+    return this.balance;
   }
 }
