d86166eb6daf7a3a1f62eea4c037201acbf7909d
jlautman <12954173+jlautman@users.noreply.github.com>
2017-11-08 19:22:58 -0500

    Build basic bank without any crypto

--- Bank/src/main/java/edu/upenn/cis551/pncbank/bank/AccountManager.java
+++ Bank/src/main/java/edu/upenn/cis551/pncbank/bank/AccountManager.java
@@ -3,22 +3,27 @@ package edu.upenn.cis551.pncbank.bank;
 import java.util.HashMap;
 import java.util.Map;
 
-public class AccountManager<D> {
-  /**
-   * Singleton instance of the account manager.
-   */
-  @SuppressWarnings({"rawtypes", "unchecked"})
-  static final AccountManager INSTANCE = new AccountManager(new HashMap<String, Account>());
+public class AccountManager {
+  Map<String, Account> accounts;
 
-  final Map<String, Account<D>> accounts;
-
-  private AccountManager(Map<String, Account<D>> accounts) {
-    this.accounts = accounts;
+  public AccountManager() {
+    this.accounts = new HashMap<String, Account>();
   }
-  
-  public boolean deposit(String accountId, String validation, long value) {
-    Account<D> account = this.accounts.get(accountId);
-    if(null == account) return false;
-    return account.updateValue(value, validation);
+
+  public synchronized boolean createAccount(String accountId, String validation, long val) {
+    if (this.accounts.containsKey(accountId)) {
+      return false;
+    } else {
+      this.accounts.put(accountId, new Account(val, validation));
+      return true;
+    }
+  }
+
+  public boolean changeValue(String accountId, String validation, long delta) {
+    Account account = this.accounts.get(accountId);
+    if (null == account) {
+      return false;
+    }
+    return account.updateValue(delta, validation);
   }
 }
--- Bank/src/main/java/edu/upenn/cis551/pncbank/bank/atmservice/AtmService.java
+++ Bank/src/main/java/edu/upenn/cis551/pncbank/bank/atmservice/AtmService.java
@@ -6,6 +6,7 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.upenn.cis551.pncbank.bank.AccountManager;
 import edu.upenn.cis551.pncbank.transaction.AbstractTransaction;
 
 /**
@@ -18,35 +19,43 @@ import edu.upenn.cis551.pncbank.transaction.AbstractTransaction;
 public class AtmService {
   private ServerSocket socketBinding;
   private ObjectMapper mapper;
+  private String aesKey;
 
-  public AtmService(int port) throws IOException {
+  public AtmService(int port, String aesKey) throws IOException {
     socketBinding = new ServerSocket(port);
     mapper = new ObjectMapper();
+    this.aesKey = aesKey;
   }
 
   /**
    * Handles the next request from the port.
    * 
-   * @return
-   * @throws IOException
    */
-  public void handleNextRequest() throws IOException {
-    Socket s = socketBinding.accept();
-    InputStream is = s.getInputStream();
-    // TODO Decryption of the data from the atm goes here
-    try {
-      AbstractTransaction at = mapper.readValue(is, AbstractTransaction.class);
+  public void handleNextRequest(AccountManager am) {
+    try (Socket s = socketBinding.accept()) {
+      InputStream is = s.getInputStream();
+      // TODO Decryption of the data from the atm goes here
+      try {
+        AbstractTransaction at = mapper.readValue(is, AbstractTransaction.class);
+      } catch (IOException e) {
+        // TODO replace with the correct error message
+        sendEncryptedResponse(s.getOutputStream(), "failure");
+        return;
+      }
+      // TODO Do stuff with the account manager here and build the actual response
+      String response = "{}";
+
+      sendEncryptedResponse(s.getOutputStream(), response);
+      s.close();
     } catch (IOException e) {
-      // TODO
+      // TODO log it?
     }
-    // TODO Do stuff with the account store here
-    
-    String response = "{}";
-    // TODO actually encrypt
-    byte[] encryptedResponse = response.getBytes();
-    OutputStream os = s.getOutputStream();
-    os.write(encryptedResponse);
+  }
+
+  private void sendEncryptedResponse(OutputStream os, String unencrypted) throws IOException {
+    // TODO replace this with the real encryption
+    byte[] data = unencrypted.getBytes();
+    os.write(data);
     os.flush();
-    s.close();
   }
 }
