2115b1a3537c4f6099b82479c07dd393fe5dcb90
jlautman <12954173+jlautman@users.noreply.github.com>
2017-11-08 19:54:39 -0500

    Finish first pass (untested) Account Manager

--- Bank/src/main/java/edu/upenn/cis551/pncbank/bank/Account.java
+++ Bank/src/main/java/edu/upenn/cis551/pncbank/bank/Account.java
@@ -16,13 +16,27 @@ public class Account {
    */
   private BigInteger balance;
   private String cardValidator;
+  private long sequence;
 
-  public Account(long balance, String validator) {
+  public Account(long balance, String validator, long sequence) {
     this.balance = BigInteger.valueOf(balance);
     this.cardValidator = validator;
+    this.sequence = sequence;
   }
 
-  public boolean updateValue(long amount, String validation) {
+  /**
+   * 
+   * @param amount The change to make on the account value
+   * @param validation The string to validate using the account's cardValidator.
+   * @param sequence The sequence number that the request sent.
+   * @return false iff the wrong sequence number is found, the validation fails, or the amount
+   *         results in a negative balance.
+   */
+  public boolean updateValue(long amount, String validation, long sequence) {
+    if (sequence != this.sequence) {
+      // Wrong sequence number.
+      return false;
+    }
     // TODO use the validation with this account's card validator to ensure the transaction is
     // allowed.
     BigInteger newVal = this.balance.add(BigInteger.valueOf(amount));
@@ -31,5 +45,9 @@ public class Account {
     }
     this.balance = newVal;
     return true;
+  }
+
+  public long getSequence() {
+    return this.sequence;
   }
 }
--- Commons/src/main/java/edu/upenn/cis551/pncbank/transaction/DepositPOJO.java
+++ Commons/src/main/java/edu/upenn/cis551/pncbank/transaction/DepositPOJO.java
@@ -2,13 +2,19 @@ package edu.upenn.cis551.pncbank.transaction;
 
 public class DepositPOJO extends AbstractTransaction {
   final long deposit;
+  final String validation;
 
-  public DepositPOJO(String accountName, long amount, long sequenceNumber) {
+  public DepositPOJO(String accountName, String validation, long amount, long sequenceNumber) {
     super(accountName, sequenceNumber);
     this.deposit = amount;
+    this.validation = validation;
   }
 
   public final long getDeposit() {
     return this.deposit;
+  }
+
+  public final String getValidation() {
+    return this.validation;
   }
 }
