208c262ed8f0fa3a4dd002b72e197fbf94c3d77c
John Lake <johnlake@jfl-mac.local>
2017-12-03 12:38:14 -0500

    Fix 1: command line arguments

--- fix/code/build/AtmBuild/src/main/java/edu/upenn/cis551/pncbank/Main.java
+++ fix/code/build/AtmBuild/src/main/java/edu/upenn/cis551/pncbank/Main.java
@@ -27,8 +27,11 @@ public class Main {
 
 
   public static void setupOptions(Options options) {
-    options.addOption("a", true, "The customer's account name");
-   // options.addOption("s", true, "The authorization file");
+
+    Option accName = new Option("a", true, "The customer's account name");
+    accName.setRequired(true);
+    options.addOption(accName);
+    options.addOption("s", true, "The authorization file");
     options.addOption("i", true, "The IP address for this server to run on");
     options.addOption("p", true, "The port for this server to run on");
     options.addOption("c", true, "The customer's ATM card file");
@@ -41,7 +44,7 @@ public class Main {
   public static void setDefaults() {
     port = 3000;
     IP = "127.0.0.1";
-    //authFile = "/Users/ryanhub/Desktop/break/teams/12/build/BankBuild/bank.auth";
+    authFile = "bank.auth";
     accountName = "";
     cardFile = "";
     authKey = null;
@@ -51,36 +54,82 @@ public class Main {
       throws ParseException, IOException {
 
 
-    CommandLineParser parser = new DefaultParser();
-    // Attempt to parse the options:
 
+    for (int i = 0; i < args.length - 1; i++) {
+      String arg = args[i];
+
+      // When parsing options that accept strings, prepend an underscore to the string.
+      if ((arg.equals("-s") || arg.equals("-a") || arg.equals("-c"))) {
+
+        StringBuilder argChanger = new StringBuilder();
+        argChanger.append("_");
+        argChanger.append(args[i + 1]);
+        args[i + 1] = argChanger.toString();
+
+        // When parsing -g, the only acceptable next argument is another option.
+      } else if (arg.equals("-g")) {
+        if (!args[i + 1].equals("-s") && !args[i + 1].equals("-i") && !args[i + 1].equals("-p")
+            && !args[i + 1].equals("-c") && !args[i + 1].equals("-n") && !args[i + 1].equals("-d")
+            && !args[i + 1].equals("-w") && !args[i + 1].equals("-g"))
+          System.exit(255);
+      }
+    }
+
+    CommandLineParser parser = new DefaultParser();
+
+
+    // Attempt to parse the options:
+    /*
+     * System.err.println("Parsing options."); for (String s : args) { System.err.println("Arg: " +
+     * s); }
+     */
     CommandLine cmd = parser.parse(options, args);
+
+
+
+    /*
+     * for (Option o : cmd.getOptions()) { System.err.println("Option: " + o.getOpt());
+     * System.err.println("Value: " + o.getValue()); }
+     */
 
     // Checks if this is a proper transaction.
     if (!InputValidator.properTransaction(cmd)) {
+      System.err.println("Invalid transaction.");
       System.exit(255);
     }
 
     // ACCOUNTNAME:
     if (!cmd.hasOption("a")) {
+      System.err.println("No accountname given.");
       System.exit(255);
     } else {
       accountName = cmd.getOptionValue("a");
+      accountName = accountName.substring(1);
+
       if (!InputValidator.isValidAccountName(accountName)) {
+        System.err.println("Invalid account name: " + accountName);
         System.exit(255);
       }
 
       // Default card file
       cardFile = accountName + ".card";
+      cardFile = cardFile.substring(1);
+
     }
 
 
     // AUTHFILE AND KEY:
-/*    authFile = cmd.getOptionValue("s", "bank.auth");
+    authFile = cmd.getOptionValue("s", "bank.auth");
+    authFile = authFile.substring(1);
     if (!InputValidator.isValidFile(authFile)) {
       System.exit(255);
     }
-    authKey = Authentication.getAESKeyFromAuthFile(authFile);*/
+    System.err.println("Getting key.");
+
+
+
+    authKey = Authentication.getAESKeyFromAuthFile(authFile);
+
 
     // IP ADDRESS:
     if (cmd.hasOption("i")) {
@@ -108,14 +157,13 @@ public class Main {
         System.exit(255);
       }
     }
+    System.err.println("Parsing complete.");
     return cmd;
   }
 
   public static void main(String[] args) {
     // Add options:
     Options options = new Options();
-    //Option option = new Option("a","account name");
-
     setupOptions(options);
 
     // Default values for options:
@@ -137,11 +185,14 @@ public class Main {
 
 
       // Have the ATM perform the correct action.
+      System.err.println("accountName: " + accountName);
       Atm a = new Atm(cmd, IP, port, cardFile, accountName, authKey);
       // Keep trying as long as the command returns false. May call System.exit to exit the jvm.
       while (!a.runCommand());
 
     } catch (ParseException | IOException | NoRequestException e) {
+      System.err.println("Error parsing.");
+      System.err.println(e.getMessage());
       System.exit(255);
     }
 
